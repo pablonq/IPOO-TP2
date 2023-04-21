31dcb2dae0634baf8e2d421b373de0aa8565deb1
PABLO NAVARRO <118768679+pablonq@users.noreply.github.com>
2023-04-20 23:16:09 -0300

    Add files via upload

---
+++ responsablev.php
@@ -0,0 +1,51 @@
+<?php
+class ResponsableV{
+private $numEmpleado ;
+private $numLicencia ; 
+private $nombre;
+private $apellido;
+
+public function __construc($numEmpleado, $numLicencia, $nombre, $apellido){
+    $this->numEmpleado =$numEmpleado ;
+    $this->numLicencia =$numLicencia ;
+    $this->nombre =$nombre ;
+    $this->apellido =$apellido ;
+    
+}
+
+public function getNumEmpleado(){
+    return $this->numEmpleado ;
+}
+
+public function setNumEmpleado($numEmpleado){
+    $this->numEmpleado =$numEmpleado ;
+}
+public function getNumLicencia(){
+    return $this->numLicencia ;
+}
+
+public function setNumLicencia($numLicencia){
+    $this->numLicencia =$numLicencia ;
+}
+public function getNombre(){
+    return $this->nombre ;
+}
+
+public function setNombre($nombre){
+    $this->nombre =$nombre ;
+}
+public function getApellido(){
+    return $this->apellido ;
+}
+
+public function setApellido($apellido){
+    $this->apellido =$apellido ;
+}
+public function __toString(){
+    return "Nombre: " . $this->getNombre() .
+    "\nApellido: " . $this->getApellido() . 
+    "\nNro Empleado: " . $this->getNumEmpleado(). 
+    "\nNro Licencia: " . $this->getNumLicencia() ;
+
+}
+}
